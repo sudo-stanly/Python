f4e099fa4572b3e4f223426ca1d467146022009d
stanly-sudo <alys.git24@gmail.com>
2025-05-26 12:36:58 +0800

    puzzle tasks

--- 01_basics/variables/puzzle/task_01.py
+++ 01_basics/variables/puzzle/task_01.py
@@ -1,4 +1,18 @@
 #[SWAP SVALUES]
-a,b,c = 'apple', 'banana', 'cherry'
-a, b, c = c,a,b
-print("a: ",a,"\nb: ",b,"\nc: ",c)
+    # a,b,c = 'apple', 'banana', 'cherry'
+    # a, b, c = c,a,b
+    # print("a: ",a,"\nb: ",b,"\nc: ",c)
+    
+#[MYSTERY COMBINATION]: You are given 3 variables representing parts of a code. Your job is to rearrange or combine them into a readable secret message.
+    # p1 = "the fox"
+    # p2 = "quick"
+    # p3 = "jumps"
+    # w1, w2 = p1.split()
+
+    # Using only variables and string manipulation, print: "the quick fox jumps"
+    # print(f"{w1} {p2} {w2} {p3}")
+    
+#[SECRET SWAP]: You have two variables holding numbers. Swap their values without using a temporary variable. Then print both variables.
+a,b = 5, 10
+a , b = b, a
+print(f"{a}\n{b}")
