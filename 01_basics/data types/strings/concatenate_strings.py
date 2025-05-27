b6597e9a6bcab297565c351aa306ca186249051f
stanly-sudo <alys.git24@gmail.com>
2025-05-27 15:02:54 +0800

    concatenate string

---
+++ 01_basics/data types/strings/concatenate_strings.py
@@ -0,0 +1,11 @@
+#[STRING CONCATENATION]
+
+#to concatenate or combine two strings you can use the '+' operator
+    # a,b='hello','world'
+    # c=a+b
+    # print(c)
+    
+#to add a space between them add a " "
+    # a,b='hello','world'
+    # c=a+ ' ' +b
+    # print(c)
