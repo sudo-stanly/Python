86feddf5b090d2b951337d93e08ce59c3247b436
stanly-sudo <alys.git24@gmail.com>
2025-05-27 14:52:11 +0800

    string methods

---
+++ 01_basics/data types/strings/string methods/modify_strings.py
@@ -0,0 +1,23 @@
+#[MODIFY STRINGS]
+
+#[UPPER CASE] upper() method :  method returns the string in upper case:
+    # a="Hello, world"
+    # print(a.upper())
+
+#[LOWER CASE] lower() method : method returns the string in lower case:
+    # a="Hello, world"
+    # print(a.lower())
+    
+# [REMOVE WHITESPACE] strip() method : method removes any whitespace from the beginning or the end: Whitespace is the space before and/or after the actual text, and very often you want to remove this space.
+    # a=" Hello, world "
+    # print(a.strip())
+    
+#[REPLACE STRING]: replace(<original>, <new>) method: The replace() method replaces a string with another string:
+    # a=" Hello, world "
+    # print(a.replace("Hello", "suzu"))
+    
+#[SPLIT STRING]: The split() method returns a list where the text between the specified separator becomes the list items.
+    #a="Hello, world"
+    #print(a.split(",")) #returns ['Hellow', 'world']
+    
+    
