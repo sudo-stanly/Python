ffe9e0f7716fb11edf6ebd873e68d9b37ebc1e15
stanly-sudo <alys.git24@gmail.com>
2025-05-29 13:15:54 +0800

    fixed 1 decimal place

--- 01_basics/data types/task/task_01.py
+++ 01_basics/data types/task/task_01.py
@@ -7,6 +7,6 @@
     Passed     : True
 """
 #3.Apply and print using string methods.
-    # n,c,s,p="\tname\t:\t{name}\n","\tcourse\t:\t{course}\n","\tscore\t:\t{score}\n","\tpassed\t:\t{passed}"
-    # x,y,z,w=n.format_map({"name":"suzu"}).title(), c.format_map({"course":"mathemathics"}).title(), s.format_map({"score":98.45}), p.format_map({"passed":True})
-    # print(x,y,z,w)
+n,c,s,p="\tname\t:\t{name}\n","\tcourse\t:\t{course}\n","\tscore\t:\t{score:.1f}\n","\tpassed\t:\t{passed}"
+x,y,z,w=n.format_map({"name":"suzu"}).title(), c.format_map({"course":"mathemathics"}).title(), s.format_map({"score":98.45}), p.format_map({"passed":True})
+print(x,y,z,w)
