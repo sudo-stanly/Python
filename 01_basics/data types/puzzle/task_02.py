12620418e9c76eb501b590294bd04ffffef9c254
stanly-sudo <alys.git24@gmail.com>
2025-05-29 16:47:53 +0800

    commented

--- 01_basics/data types/puzzle/task_02.py
+++ 01_basics/data types/puzzle/task_02.py
@@ -1,5 +1,5 @@
 #[HIDDEN MESSAGE]: The hidden word is made of every second character, starting at index 1
     #output: python rules
     
-secret = "pynothelrsu"
-print(f"{secret[0:2]}{secret[4:6]}{secret[3:2]}{secret[3]}{secret[2]} {secret[8]}{secret[10]}{secret[7]}{secret[6]}{secret[9]}")
+    # secret = "pynothelrsu"
+    # print(f"{secret[0:2]}{secret[4:6]}{secret[3:2]}{secret[3]}{secret[2]} {secret[8]}{secret[10]}{secret[7]}{secret[6]}{secret[9]}")
