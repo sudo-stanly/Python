c2410d9736bec8f152f44de243ffa1fbf172bf48
stanly-sudo <alys.git24@gmail.com>
2025-05-29 16:39:43 +0800

    data type puzzle

---
+++ 01_basics/data types/puzzle/task_01.py
@@ -0,0 +1,8 @@
+#[NAME MIXER]: Task: Create a new string like this => "A.L."
+    # Use string indexing and formatting
+
+# first, last ="Ada", "Lovelace"
+        # ff=first.find("A") #0
+        # fl=last.find("L") #0
+        # print(ff); print(fl)
+# print(f"{first[0]}.{last[0]}.")
