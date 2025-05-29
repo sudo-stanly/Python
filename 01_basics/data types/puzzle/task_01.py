f499d99b0649bfbc707d180dc021757b337c399c
stanly-sudo <alys.git24@gmail.com>
2025-05-29 16:48:52 +0800

    data type puzzle

--- 01_basics/data types/puzzle/task_01.py
+++ 01_basics/data types/puzzle/task_01.py
@@ -1,4 +1,4 @@
-#[NAME MIXER]: Task: Create a new string like this => "A.L."
+#3[NAME MIXER]: Task: Create a new string like this => "A.L."
     # Use string indexing and formatting
 
 # first, last ="Ada", "Lovelace"
@@ -6,3 +6,10 @@
         # fl=last.find("L") #0
         # print(ff); print(fl)
 # print(f"{first[0]}.{last[0]}.")
+
+
+#2[HIDDEN MESSAGE]: The hidden word is made of every second character, starting at index 1
+    #output: python rules
+    
+    # secret = "pynothelrsu"
+    # print(f"{secret[0:2]}{secret[4:6]}{secret[3:2]}{secret[3]}{secret[2]} {secret[8]}{secret[10]}{secret[7]}{secret[6]}{secret[9]}")
