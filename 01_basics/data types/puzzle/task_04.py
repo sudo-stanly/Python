40f56614d38b90e43642873dfc9a07139e41e795
stanly-sudo <alys.git24@gmail.com>
2025-05-30 14:53:35 +0800

    data type task

---
+++ 01_basics/data types/puzzle/task_04.py
@@ -0,0 +1,5 @@
+#[SPACE]:A mysterious file stores records with hidden whitespace-only lines. Identify all the entries that contain only whitespace.
+    # entries = ["   ", "\t\n", "data", "  .", "", " "]  # → ?
+    # for i in range(0, 6):
+    #     if(entries[i].isspace()):
+    #         print(entries[i])
