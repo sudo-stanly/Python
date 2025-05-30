1229b01ab2578b064eeaabb1a5451ea8b2aec744
stanly-sudo <alys.git24@gmail.com>
2025-05-30 15:41:58 +0800

    data type puzzle task-04 'decimal characters only'

--- 01_basics/data types/puzzle/task_04.py
+++ 01_basics/data types/puzzle/task_04.py
@@ -2,3 +2,8 @@
     # l1=["100", "3.0", "⅔", "42", "eight", ""]
     # r1=list(map(str.isdecimal, l1))
     # print(r1)
+    
+#decimal characters only
+    # l2=["٢", "4.5", "5", "٩", "0", ""]
+    # r2=list(map(str.isdecimal, l2))
+    # print(r2)
