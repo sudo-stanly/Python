a5d7f79225ebe94f6b62012d1f3e6e0e1b28ff65
stanly-sudo <alys.git24@gmail.com>
2025-05-30 15:24:33 +0800

    data type task

--- 01_basics/data types/puzzle/task_03.py
+++ 01_basics/data types/puzzle/task_03.py
@@ -2,3 +2,8 @@
     # l1 = ["42", "3.14", "٠١٢", "1000", "ten", ""]
     # r1 = list(map(str.isdecimal, l1))
     # print(r1)
+    
+#Which strings are strictly lowercase?
+    # l2 = ["apple", "Apple", "BANANA", "banana", "Cherry", ""]
+    # r2 = list(map(str.islower, l2))
+    # print(r2)
