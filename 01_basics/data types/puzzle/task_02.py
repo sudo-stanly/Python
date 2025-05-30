741f6cd352b4c525f8afeab46ce66d372e7db256
stanly-sudo <alys.git24@gmail.com>
2025-05-30 14:51:19 +0800

    data type task

--- 01_basics/data types/puzzle/task_02.py
+++ 01_basics/data types/puzzle/task_02.py
@@ -1,3 +1,3 @@
 #[CHECK FOR DECIMAL NUMBER]:You are given a list of mysterious codes. Determine for each whether it consists solely of decimal characters.
-codes = ["12345", "007", "9⅞", "42.0", "٣", ""]
-print(codes[0].isdecimal(), codes[1].isdecimal(), codes[2].isdecimal(), codes[3].isdecimal(), codes[4].isdecimal(), codes[5].isdecimal())
+    # codes = ["12345", "007", "9⅞", "42.0", "٣", ""]
+    # print(codes[0].isdecimal(), codes[1].isdecimal(), codes[2].isdecimal(), codes[3].isdecimal(), codes[4].isdecimal(), codes[5].isdecimal())
--- 01_basics/data types/puzzle/task_03.py
+++ 01_basics/data types/puzzle/task_03.py
@@ -1,1 +1,5 @@
 #[WHISPER GAME]:A secret message list has been whispered in lowercase. Confirm whether each string is fully in lowercase, with no uppercase surprises.
+    # messages = ["quiet", "LOUD", "whisPer", "shhh", "lower", ""]  # → ?
+    # for i in range(0, 6):
+    #     if(messages[i].islower()):
+    #         print(messages[i])
